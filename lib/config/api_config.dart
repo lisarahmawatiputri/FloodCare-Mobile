d574970cec7fc01426283981157b0d2b1ecbb51e
salw89 <lisarahmawati294@gmail.com>
2026-04-30 20:48:30 +0700

    test

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -8,7 +8,7 @@ class ApiConfig {
     }
 
     if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8000/api';
+      return 'http:// 10.43.140.102:8000/api';
     }
 
     return 'http://127.0.0.1:8000/api';
